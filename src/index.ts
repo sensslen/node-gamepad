74eb9f38333b3954ac0970d27e391a00bc07f17c
Simon Ensslen <simon.ensslen@griesser.ch>
2021-02-08 17:50:03 +0100

    do publish in dist folder to get rid of unnecessary folder nesting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { RumblingDualshock4 } from './controllers/ps4/RumblingDualshock4';
+import { IConfig } from './IConfig';
+import { ILogger } from './ILogger';
 import { NodeGamepad } from './NodeGamepad';
 
 export { NodeGamepad };
 export { RumblingDualshock4 };
+export { ILogger };
+export { IConfig };
