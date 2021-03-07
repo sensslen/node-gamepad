1e7c05cab629ed8ce4902be7d3de6339cf9000a3
Simon Ensslen <simon.ensslen@griesser.ch>
2021-03-07 13:07:14 +0100

    - add simple test - version 0.2.0

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+import { ILogger, NodeGamepad } from '.';
+import * as config from './controllers/logitech/gamepadf310.json';
+
+let logger: ILogger = {
+    Log(toLog: string) {
+        console.log(toLog);
+    },
+};
+
+let gamepad = new NodeGamepad(config, logger);
+gamepad.start();
+
+gamepad.on('dpadUp:press', () => console.log(`dpadUp:true`));
+gamepad.on('dpadUp:release', () => console.log(`dpadUp:false`));
