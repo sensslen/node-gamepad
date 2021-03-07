9ad2e8357d9c3703d6c8481d115ede94e7d8187d
Simon Ensslen <simon.ensslen@griesser.ch>
2021-03-07 14:00:19 +0100

    use git to automatically calculate the appropriate version number

--- src/SetupPackage.ts
+++ src/SetupPackage.ts
@@ -1,10 +1,18 @@
 import fs from 'fs';
+import path from 'path';
+
+import simpleGit, { SimpleGit } from 'simple-git';
+const git: SimpleGit = simpleGit();
 
 // DO NOT DELETE THIS FILE
 // This file is used by build system to build a clean npm package with the compiled js files in the root of the package.
 // It will not be included in the npm package.
 
-function main() {
+async function calculateVersionNumber() {
+    return await git.raw('describe', '--tags', '--dirty', '--always');
+}
+
+async function main() {
     const source = fs.readFileSync(__dirname + '/../package.json').toString('utf-8');
     const sourceObj = JSON.parse(source);
     sourceObj.scripts = {};
@@ -15,6 +23,9 @@ function main() {
     if (sourceObj.types.startsWith('dist/')) {
         sourceObj.types = sourceObj.types.slice(5);
     }
+
+    sourceObj.version = (await calculateVersionNumber()).trim();
+
     fs.writeFileSync(__dirname + '/package.json', Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8'));
 
     fs.copyFileSync(__dirname + '/../.npmignore', __dirname + '/.npmignore');
