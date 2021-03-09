0590fadcda72e73a2f9f2375d53dc1b9f0810026
Simon Ensslen <simon.ensslen@griesser.ch>
2021-03-09 20:21:15 +0100

    correct publish preparation script

--- src/SetupPackage.ts
+++ src/SetupPackage.ts
@@ -13,7 +13,7 @@ async function calculateVersionNumber() {
     const gitDescribeVersion = await git.raw('describe', '--tags', '--dirty', '--always');
     const split = gitDescribeVersion.split('-');
     if (split.length == 1) {
-        return split[1];
+        return split[0];
     }
 
     const betaCount = Number(split[1]);
