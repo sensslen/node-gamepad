6c95918f7817a24037399d6ba8cdaaa7830776e5
Chrischona Frauenfeld <info@chrischona-frauenfeld.ch>
2021-03-13 12:04:25 +0100

    correct test app

--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,19 @@ import { ILogger, NodeGamepad } from '.';
 import * as config from './controllers/logitech/gamepadf310.json';
 
 let logger: ILogger = {
-    Log(toLog: string) {
+    Info(toLog: string) {
+        console.log(toLog);
+    },
+    Debug(toLog: string) {
+        console.log(toLog);
+    },
+    DebugLowLevel(toLog: string) {
         console.log(toLog);
     },
 };
 
 let gamepad = new NodeGamepad(config, logger);
-gamepad.start(true);
+gamepad.start();
 
 gamepad.on('dpadUp:press', () => console.log(`dpadUp:true`));
 gamepad.on('dpadUp:release', () => console.log(`dpadUp:false`));
