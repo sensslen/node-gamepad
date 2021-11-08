a71e60bac56849965107efd000e7a92297b2f612
Simon Ensslen <simon.ensslen@griesser.ch>
2021-11-08 21:06:30 +0100

    update package setup

--- src/SetupPackage.ts
+++ src/SetupPackage.ts
@@ -42,10 +42,10 @@ async function main() {
     console.log(`setting version number:${version}`);
     sourceObj.version = version;
 
-    fs.writeFileSync(__dirname + '/package.json', Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8'));
+    fs.writeFileSync(path.join(__dirname, 'package.json'), Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8'));
 
-    fs.copyFileSync(__dirname + '/../.npmignore', __dirname + '/.npmignore');
-    fs.copyFileSync(__dirname + '/../README.md', __dirname + '/README.md');
+    fs.copyFileSync(path.join(__dirname, '../.npmignore'), path.join(__dirname, '.npmignore'));
+    fs.copyFileSync(path.join(__dirname, '../README.md'), path.join(__dirname, 'README.md'));
 }
 
 main();
